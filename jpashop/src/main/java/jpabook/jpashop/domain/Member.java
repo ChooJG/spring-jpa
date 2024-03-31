bca3cdf5c6d12f0a44373df2a4d76d5c4d6bbd64
ChooJG <101338601+ChooJG@users.noreply.github.com>
2024-03-31 15:39:42 +0900

    Add files via upload

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+    
+    @OneToMany(mappedBy = "member") //Order 테이블에 있는 member에 의해 mapped 됨
+    private List<Order> orders = new ArrayList<>();
+
+}
